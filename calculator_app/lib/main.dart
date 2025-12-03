e8866ea4fe78ccb9e4a3697c73c22bfbb4ff6fa2
Jesper <jesper.hunesjo@hunet.se>
2025-12-03 17:26:08 +0100

    Responsive layout

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -1,4 +1,12 @@
+import 'package:calculator_app/models/model.dart';
+import 'package:calculator_app/responsive/responsive_layout.dart';
+import 'package:calculator_app/utils/expression_handler.dart';
+import 'package:calculator_app/utils/utils.dart';
 import 'package:calculator_app/views/home_page.dart';
+import 'package:calculator_app/widgets/display_result.dart';
+import 'package:calculator_app/widgets/history.dart';
+import 'package:calculator_app/widgets/keypad.dart';
+import 'package:calculator_app/widgets/transparent_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -18,12 +26,147 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.white)),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Kalkylator"),),
-        backgroundColor: Colors.white,
-        body: SafeArea(child: HomePage()),
+      home: ResponsiveLayout(mobileBody: HomePage(), desktopApp: DesktopHomePage()),
+    );
+  }
+}
+
+class DesktopHomePage extends StatefulWidget {
+  const DesktopHomePage({super.key});
+
+  @override
+  State<DesktopHomePage> createState() => _DesktopHomePageState();
+}
+
+class _DesktopHomePageState extends State<DesktopHomePage> {
+  String expressionText = "0";
+  String? resolutionText;
+  bool calculated = false;
+  bool isHistoryOpen = false;
+  List<HistoryItem> calculationHistory = [];
+
+  void handleKeypadPress(String value) {
+    setState(() {
+      if (expressionText == "0" || calculated) {
+        expressionText = value;
+        calculated = false;
+        resolutionText = null;
+      } else {
+        expressionText += value;
+      }
+    });
+  }
+
+  void onCalculate() {
+    String expression = expressionText;
+    try {
+      num res = evaluateExpression(expressionText);
+      setState(() {
+        expressionText = res.toString();
+        resolutionText = expression;
+        calculated = true;
+        calculationHistory.add(
+          HistoryItem(
+            result: res.toString(),
+            expression: resolutionText!,
+            date: getCurrentDate(),
+          ),
+        );
+      });
+    } catch (e) {
+      setState(() {
+        expressionText = "";
+        resolutionText = "Ogiltigt uttryck";
+      });
+    }
+  }
+
+  void onClear() {
+    setState(() {
+      expressionText = "0";
+      resolutionText = null;
+    });
+  }
+
+  void onClearEntry() {
+    setState(() {
+      if (expressionText.length > 1) {
+        expressionText = expressionText.substring(0, expressionText.length - 1);
+      }
+    });
+  }
+
+  void onClearHistory() {
+    setState(() {
+      calculationHistory.clear();
+    });
+  }
+  final horizontalPadding = 20.0;
+
+  final verticalPadding = 20.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SizedBox(
+          width: 1600,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    horizontalPadding,
+                    verticalPadding,
+                    horizontalPadding,
+                    8.0,
+                  ),
+                  child: DisplayResult(
+                    expression: expressionText,
+                    result: resolutionText,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: horizontalPadding,
+                  vertical: 8.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        TransparentButton(text: "C", onTap: onClear),
+                        SizedBox(width: 8),
+                        TransparentButton(text: "CE", onTap: onClearEntry),
+                      ],
+                    ),
+                    HistoryWidget(
+                      history: calculationHistory,
+                      onClearHistory: onClearHistory,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: EdgeInsets.all(horizontalPadding),
+                  child: Keypad(
+                    onPressed: handleKeypadPress,
+                    onCalculate: onCalculate,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- calculator_app/lib/widgets/transparent_button.dart
+++ calculator_app/lib/widgets/transparent_button.dart
@@ -8,27 +8,41 @@ class TransparentButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final fontSize = size.width < 600 ? 24.0 : 32.0;
+    final screenWidth = MediaQuery.of(context).size.width;
+    double fontSize;
+    if (screenWidth < 600) {
+      fontSize = 32.0;
+    } else if (screenWidth < 900) {
+      fontSize = 48.0;
+    } else if (screenWidth < 1400) {
+      fontSize = 48.0;
+    } else if (screenWidth > 1500) {
+      fontSize = 99.0;
+    } else {
+      fontSize = 26.0;
+    }
+    final iconSize = fontSize;
+    final padding = fontSize / 4;
+
     if (icon != null) {
       return GestureDetector(
         onTap: onTap,
         child: Padding(
-          padding: EdgeInsetsGeometry.all(20),
-          child: Icon(icon),
+          padding: EdgeInsets.all(padding),
+          child: Icon(icon, size: iconSize, color: Colors.grey[700]),
         ),
       );
     } else {
       return GestureDetector(
         onTap: onTap,
         child: Padding(
-          padding: EdgeInsetsGeometry.all(20),
+          padding: EdgeInsets.all(padding),
           child: Text(
             text,
             style: TextStyle(
               fontSize: fontSize,
               fontWeight: FontWeight.w600,
-              color: Colors.grey[500],
+              color: Colors.grey[700],
             ),
           ),
         ),
